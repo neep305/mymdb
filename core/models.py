fa02ff9498540d129f070b3f76045a7a007d824b
neep305 <neep305@gmail.com>
2019-10-10 03:03:11 +0900

    add a new model : Person

--- core/models.py
+++ core/models.py
@@ -27,3 +27,27 @@ class Movie(models.Model):
 
     def __str__(self):
         return '{} ({})'.format(self.title, self.year)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=140)
+    last_name = models.CharField(max_length=140)
+    born = models.DateField()
+    died = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('last_name', 'first_name')
+
+    def __str__(self):
+        if self.died:
+            return '{}, {}, ({}-{})'.format(
+                self.last_name,
+                self.first_name,
+                self.born,
+                self.died
+            )
+        return '{}, {}, ({})'.format(
+            self.last_name,
+            self.first_name,
+            self.born
+        )
